9afd7345e806b0652110507581bd63f1aa0dbd10
flores-97 <floresvictor15@gmail.com>
2025-01-24 16:21:25 -0800

    Add project files.

---
+++ COMP003A.LectureActivity3.4/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+ Author: Victor Flores
+Course: COMP-003A
+Faculty: Jonathon Cruz
+Purpose: Demonstrate iterative statements in C#*/
+namespace COMP003A.LectureActivity3._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
